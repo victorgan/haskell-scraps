f81eb572f52caae78d6efd6d04ea9e1d5426a26d
Victor Gan <vigan@VIGAN-3-1013.redmond.corp.microsoft.com>
2016-02-02 12:27:03 -0800

    sum 4th column of csv file

--- bassbull/src/Main.hs
+++ bassbull/src/Main.hs
@@ -1,3 +1,18 @@
-Main where
+module Main where
 
-main = putStrLn "hello"
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+-- from cassava
+import Data.Csv
+
+-- a simple type alias for data
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
+summer (name, year, team, atBats) n = n + atBats
+
+main :: IO ()
+main = do
+  csvData <- BL.readFile "batting.csv"
+  let v = decode NoHeader csvData :: Either String (V.Vector BaseballStats)
+  let summed = fmap (V.foldr summer 0) v
+  putStrLn $ "Total atBats was: " ++ (show summed)

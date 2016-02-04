c3421e36a1feacf87c46a0c70182b1e6dd86647e
Victor Gan <victorgan@gmail.com>
2016-02-04 08:47:59 -0800

    Various

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,19 @@
 module Main where
 
-main = putStrLn "hello"
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+
+-- from cassava
+import Data.Csv
+
+-- a simple type alias for data, representing one row from the CSV
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
+main :: IO ()
+main = do
+    csvData <- BL.readFile "batting.csv"
+    let v = decode NoHeader csvData :: Either String (V.Vector BaseballStats)
+    let summed = fmap (V.foldr summer 0) v
+    putStrLn $ "Total atBats was: " ++ (show summed)
+    where summer (name, year, team, atBats) n = n + atBats
+

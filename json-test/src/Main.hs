7729d5dfe77759d39cb95935f615f3fb1d40998d
Victor Gan <victorgan@gmail.com>
2016-02-13 03:28:26 -0800

    Output multiple activities for a single user

--- json-test/src/Main.hs
+++ json-test/src/Main.hs
@@ -50,6 +50,7 @@ capitalizeFirst [] = []
 -- Instances to convert our type to JSON.
 type TenantId = T.Text
 type UserNumber = Integer
+type ActivityNumber = Integer
 type EsId = T.Text
 type EsIndex = T.Text
 type EsActivityType = T.Text
@@ -57,96 +58,135 @@ type EsActivityType = T.Text
 toText :: UserNumber -> T.Text
 toText = T.pack . show
 
-makeId :: TenantId -> UserNumber -> EsId 
-makeId t i = t `T.append` "-u-" `T.append` (toText i)
+makeId :: TenantId -> UserNumber -> ActivityNumber -> EsId 
+makeId t u a = t `T.append` "-u-" `T.append` (toText u) `T.append` "-a-" `T.append` (toText a)
 
 -- Make 
 makeHeader :: EsIndex -> EsActivityType -> EsId -> Header
 makeHeader _index _type _id = Header $ HeaderContent _index _type _id
 
-makeActivityAbnormal :: TenantId -> UserNumber -> ActivityAbnormal
-makeActivityAbnormal a b = ActivityAbnormal a (toText b) (toText b) (toText b) True
-
-makeActivityBlacklist :: TenantId -> UserNumber -> ActivityBlacklist
-makeActivityBlacklist a b = ActivityBlacklist a (toText b) (toText b) (toText b) True
 
 -- Aggregate
 
-aggActivityNormal :: TenantId -> [Integer] -> [ActivityAbnormal]
-aggActivityNormal t = map (makeActivityAbnormal t)
-
--- Inputs
-filename :: FilePath
-filename = "temp.json"
-
-writeJson :: ToJSON a => a -> IO()
-writeJson toJson = B.writeFile filename $ encode toJson
-
-
-
--- Inputs
-esIndexActivities :: EsIndex
-esIndexActivities = "processedevents_v3.2015_12"
-
-esActivityTypes :: [EsActivityType]
-esActivityTypes = ["azblacklistlogin", "abnormallogin", "azdsuspiciouslogin", "compromisedcredentials", "familiarfeatureevent", "infecteddevicelogin"]
-
-realTenantId :: T.Text
-realTenantId = "tenantIddummy"
-
-
-realUserId :: Integer
-realUserId = 1;
-
-
-encodeMap :: ToJSON a => [a] -> [B.ByteString]
-encodeMap = map encode
-
-combineByteStrings :: [B.ByteString] -> B.ByteString 
-combineByteStrings = concatList . (List.intersperse "\n")
-    where concatList = foldl1 B.append
-
 encodeBulkCommand :: ToJSON a => [a] -> B.ByteString
 encodeBulkCommand = combineByteStrings . encodeMap
+    where encodeMap = map encode
 
+-------------------------------------------------------------------------------
 encodeCommand :: (ToJSON a, ToJSON b) => a -> b -> [B.ByteString]
 encodeCommand a b = [encode a, encode b];
 
 
 
 
+-------------------------------------------------------------------------------
+-- Where you can input teant id, usernumber and activitynumber
+headerAbnormal :: TenantId -> UserNumber -> ActivityNumber -> Header
+headerAbnormal t u a = makeHeader esIndexActivities esTypeAbnormal (makeId t u a)
+
+headerBlacklist :: TenantId -> UserNumber -> ActivityNumber -> Header
+headerBlacklist t u a = makeHeader esIndexActivities esTypeBlacklist (makeId t u a)
+
+contentAbnormal :: TenantId -> UserNumber -> ActivityNumber -> ActivityAbnormal
+contentAbnormal t u a = ActivityAbnormal t (toText u) (toText a) (toText u) True
+
+contentBlacklist :: TenantId -> UserNumber -> ActivityNumber -> ActivityBlacklist
+contentBlacklist t u a = ActivityBlacklist t (toText u) (toText a) (toText u) True
 
 
-headerActivity :: [B.ByteString]
-headerActivity = encodeCommand blackListHeader activityContent
+-------------------------------------------------------------------------------
+-- Apply parameters
 
-activityContent :: ActivityAbnormal
-activityContent = makeActivityAbnormal realTenantId realUserId
+-- headerAbnormal2 ::  Header
+-- headerAbnormal2 = headerAbnormal temptTenantId tempUserNumber
 
--- FINAL FORM
-type NumUsers = Integer
-type NumActivitiesPerUser = Integer
-desiredNumUsers :: NumUsers
-desiredNumUsers = 2
-desiredNumActivitiesPerUser :: NumActivitiesPerUser
+-- contentAbnormal2 :: ActivityAbnormal
+-- contentAbnormal2 = contentAbnormal desiredTenantId tempUserNumber
+
+-- commandActivity :: ToJSON a => Header -> a -> (UserNumber -> B.ByteString)
+-- commandActivity h c u = combineByteStrings $ encodeCommand (h t u) (c t u)
+--     where t = desiredTenantId :: TenantId
+
+combineByteStrings :: [B.ByteString] -> B.ByteString 
+combineByteStrings = concatList . (List.intersperse "\n")
+    where concatList = foldl1 B.append
+
+commandAbnormal :: UserNumber -> ActivityNumber -> B.ByteString
+commandAbnormal u a = combineByteStrings $ encodeCommand (headerAbnormal t u a) (contentAbnormal t u a)
+    where t = desiredTenantId :: TenantId
+
+commandsAbnormal :: B.ByteString
+commandsAbnormal = combineByteStrings $ map (commandAbnormal u) as
+    where as = activityRange :: [ActivityNumber]
+          u = 1 :: UserNumber
+
+commandBlacklist :: UserNumber -> ActivityNumber -> B.ByteString
+commandBlacklist u a = combineByteStrings $ encodeCommand (headerBlacklist t u a) (contentBlacklist t u a)
+    where t = desiredTenantId :: TenantId
+
+commandsBlacklist :: B.ByteString
+commandsBlacklist = combineByteStrings $ map (commandBlacklist u) as
+    where as = activityRange :: [ActivityNumber]
+          u = 1 :: UserNumber
+
+
+commandsActivities :: B.ByteString
+commandsActivities = combineByteStrings [commandsAbnormal, commandsBlacklist]
+
+-------------------------------------------------------------------------------
+-- Parameters
+tempUserNumber :: Integer
+tempUserNumber = 1;
+
+filename :: FilePath
+filename = "temp.json"
+
+-------------------------------------------------------------------------------
+-- Endpoint-related
+esIndexActivities :: EsIndex
+esIndexActivities = "processedevents_v3.2015_12"
+
+esTypeAbnormal :: EsActivityType
+esTypeAbnormal = "abnormallogin"
+
+esTypeBlacklist :: EsActivityType
+esTypeBlacklist = "azblacklistlogin"
+
+esActivityTypes :: [EsActivityType]
+esActivityTypes = ["azblacklistlogin", "abnormallogin", "azdsuspiciouslogin", "compromisedcredentials", "familiarfeatureevent", "infecteddevicelogin"]
+
+-------------------------------------------------------------------------------
+-- Input
+desiredTenantId :: T.Text
+desiredTenantId = "tenantIddummy"
+
+type UserCount = Integer
+type ActivityCountPerUser = Integer
+desiredUserCount :: UserCount
+desiredUserCount = 2
+
+userRange :: [UserNumber]
+userRange = [0 .. u] :: [UserNumber]
+    where u = desiredUserCount
+
+desiredNumActivitiesPerUser :: ActivityCountPerUser
 desiredNumActivitiesPerUser = 3
 
+activityRange :: [ActivityCountPerUser]
+activityRange = [1 .. desiredNumActivitiesPerUser]
 
 
-generateActivities :: NumUsers -> NumActivitiesPerUser -> IO()
-generateActivities _ _ = B.writeFile filename (combineByteStrings headerActivity)
+-------------------------------------------------------------------------------
+generateActivities :: UserCount -> ActivityCountPerUser -> IO()
+generateActivities _ _ = B.writeFile filename commandsActivities
 
 testit :: IO ()
-testit = generateActivities desiredNumUsers desiredNumActivitiesPerUser
+testit = generateActivities desiredUserCount desiredNumActivitiesPerUser
 
 -- Random tests
 
 -- Transform
-activityHeader :: EsActivityType -> TenantId -> UserNumber -> Header
-activityHeader a b c = makeHeader esIndexActivities a (makeId b c)
 
-blackListHeader :: Header
-blackListHeader = activityHeader (head esActivityTypes) realTenantId realUserId
 
 
 
